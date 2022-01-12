97445bdf8255926eeb1e46007de303428d0c26cb
jgiamporcaro-ledger <jonathan.giamporcaro@gmail.com>
2022-01-12 16:31:23 +0100

    LL-8799 - disclaimer drawer - Rebranding

--- src/screens/Platform/index.js
+++ src/screens/Platform/index.js
@@ -71,6 +71,7 @@ const PlatformCatalog = ({ route }: { route: { params: RouteParams } }) => {
           disableDisclaimer: () => setDisclaimerDisabled(),
           closeDisclaimer: () => setDisclaimerOpened(false),
           icon: manifest.icon,
+          name: manifest.name,
           onContinue: openDApp,
         });
         setDisclaimerOpened(true);
@@ -113,6 +114,7 @@ const PlatformCatalog = ({ route }: { route: { params: RouteParams } }) => {
           onContinue={disclaimerOpts.onContinue}
           isOpened={disclaimerOpened}
           icon={disclaimerOpts.icon}
+          name={disclaimerOpts.name}
         />
       )}
 
